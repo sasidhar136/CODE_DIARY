155deb50e3f11c6e3c5329f1de832993d71bf116
sai-vikas1 <chipusaivikas7@gamil.com>
2025-08-14 09:48:32 +0530

    port

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def weekly_summary():
 if __name__ == '__main__' :
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=10000,debug=True)
